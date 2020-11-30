1acf429a7e3aea5c67132b7cc1e08a913fcf48c7
leovieir4 <leovieiradevjs@gmail.com>
2020-11-30 20:06:59 -0300

    chore: add url api production on heroku

--- src/http/index.js
+++ src/http/index.js
@@ -5,7 +5,7 @@ Vue.use(VueResource)
 
 const http = Vue.http
 
-http.options.root = 'http://localhost:5050/api/'
+http.options.root = 'https://api-gorila-one.herokuapp.com/api/'
 
 Object.keys(services).map(service => {
   services[service] = Vue.resource('', {}, services[service])
