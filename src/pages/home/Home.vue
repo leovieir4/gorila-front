f1d59a08e12b9c6fca3f2ee248de307303241b1a
leovieir4 <leovieiradevjs@gmail.com>
2020-11-30 21:38:16 -0300

    refactor: remove $ from html

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -68,7 +68,7 @@
              <tbody>
                <tr v-for="ticket in  investments" v-bind:key="ticket.id">
                  <td>Renda {{ticket.type}}</td>
-                 <td>R$:${{ticket.value}}</td>
+                 <td>R$:{{ticket.value}}</td>
                  <td>{{(new Date(1606666922574).getDate()+1) + '/' + new Date(1606666922574).getMonth() + '/' + new Date(1606666922574).getFullYear()}}</td>
                  <td>{{ticket.ticket || '#'}}</td>
                  <td class="td-remove"><button type="button" class="btn btn-danger" @click="remove(ticket.id)">REMOVER</button></td>
